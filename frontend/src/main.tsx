21aab2be6917910b5e451d1e26c6d1162202f0e2
Viraj Bamne <virajbamne123@gmail.com>
2024-09-28 19:58:36 +0530

    first commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+import './global.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoute";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <Auth0ProviderWithNavigate>
+        <AppRoutes/>
+      </Auth0ProviderWithNavigate>
+      <AppRoutes/>
+    </Router>
+  </React.StrictMode>,
+);
