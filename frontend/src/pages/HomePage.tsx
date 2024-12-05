425acba9a0b3454a719190c28220b7c7e63b0a5e
Viraj Bamne <virajbamne123@gmail.com>
2024-12-05 14:57:18 +0530

    5hrs

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className="flex flex-col gap-12">
       <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-          Tuck into a takeway today
+          BOMBAY BITES
         </h1>
         <span className="text-xl">Food is just a click away!</span>
       
@@ -16,13 +16,12 @@ const HomePage = () => {
         <img src={landingImage} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <span className="font-bold text-3xl tracking-tighter">
-            Order takeaway even faster!
+            SCAN ORDER PAY
           </span>
           <span>
-            Download the MernEats App for faster ordering and personalised
-            recommendations
+            Just sit relax and Order
           </span>
-          <img src={appDownloadImage} />
+          {/* <img src={appDownloadImage} /> */}
         </div>
       </div>
     </div>
